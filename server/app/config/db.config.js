1932c0275ead4decb048fe9335d8008932196f3e
pablo014 <angelopablo014@gmail.com>
2023-07-29 06:29:28 -0700

    feat(env): added environment variables

--- server/app/config/db.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: "mongodb+srv://pablo014:N3wbkilr*@menu.gseyerc.mongodb.net/?retryWrites=true&w=majority"
-};
--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,11 +1,11 @@
-const dbConfig = require("../config/db.config.js");
+require('dotenv').config();
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = process.env.MONGO_URL;
 db.quiz = require("./quiz.model.js")(mongoose);
 
 module.exports = db;
