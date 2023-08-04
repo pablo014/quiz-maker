6831fa296616fea0aacaae2e5f34ed82ad80ce60
pablo014 <angelopablo014@gmail.com>
2023-08-03 21:06:45 -0700

    feat(layou): quiz layout view

--- client/QuizMaker/src/views/QuizView.vue
+++ client/QuizMaker/src/views/QuizView.vue
@@ -8,17 +8,28 @@ const id = ref();
 const data = ref();
 onMounted(async () => {
     id.value = route.params.id;
-    const res = await axios.get('http://localhost:8080/api/quizzes', route.params.id)
+    const res = await axios.get(`http://localhost:8080/api/quizzes/${id.value}`)
     data.value = res.data;
 })
 </script>
 
 <template>
-<div>
-    {{ data }}
+<div v-if="data">
+  <h1>{{ data.title }}</h1>
+  <div>Created by {{ data.author }}</div>
+  <div>{{ data.summary }}</div>
+  <div v-for="question in data.questions" class="list-container">
+    <div>{{ question.text }}</div>
+  </div>
 </div>
 </template>
 
 <style scoped>
-
+.list-container {
+  width: 100%;
+  border: 4px solid #f2f2f2;
+  border-radius: 10px;
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+}
 </style>
